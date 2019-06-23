d74b1cd0cb9da8245f882bea8ffa55c7f48d03bf
Timothy Lang <tlang80@gmail.com>
2019-06-23 11:01:41 -0700

    namespace name change and basic outline of Passw0rd class

--- Password Checker/Program.cs
+++ Password Checker/Program.cs
@@ -1,12 +1,27 @@
 ﻿using System;
 
-namespace Password_Checker
+namespace PasswordChecker
 {
     class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+        }
+    }
+
+    class Passw0rd
+    {
+        private readonly string password;
+
+        public Passw0rd(string password)
+        {
+            this.password = password;
+        }
+
+        public string checkPassword(string psswd)
+        {
+
         }
     }
 }
