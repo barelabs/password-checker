87f258c1770b8763e5aebe0dd5e9f6077ab1416d
Timothy Lang <tlang80@gmail.com>
2019-06-23 10:56:35 -0700

    initial commit

---
+++ Password Checker/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Password_Checker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
